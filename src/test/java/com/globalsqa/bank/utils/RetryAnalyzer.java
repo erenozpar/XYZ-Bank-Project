ac8f68235a5d370fe35d30f0186517d23cbfabf0
erenozpar <eren.ozpar@yahoo.com>
2025-07-12 15:18:26 -0500

    Added Retry Analyzer for flaky tests

--- src/test/java/com/globalsqa/bank/utils/RetryAnalyzer.java
+++ src/test/java/com/globalsqa/bank/utils/RetryAnalyzer.java
@@ -6,7 +6,7 @@ import org.testng.ITestResult;
 public class RetryAnalyzer implements IRetryAnalyzer {
 
     private int retryCount = 0;
-    private static final int MAX_RETRY_COUNT = 2;
+    private static final int MAX_RETRY_COUNT = 3;
 
     @Override
     public boolean retry(ITestResult result) {
